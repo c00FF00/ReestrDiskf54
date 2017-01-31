8e784b1eb5f87b7c2eb2126b115caab33bc9dc48
gig <grig@esso.ru>
2017-01-31 11:06:47 +0300

    View Index maked.

---
+++ resources/views/index.blade.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="/css/app.css" rel="stylesheet" type="text/css">
+    <script src="/js/app.js"></script>
+</head>
+
+<body>
+
+
+<div class="main">
+    <div class="page-header">
+        <h1>Реестр дисквалифицированных лиц</h1>
+    </div>
+    <form class="form-inline">
+        <div class="form-group">
+            <label for="Family"></label>
+            <input type="text" size="40" class="form-control" id="Family" placeholder="Фамилия">
+        </div>
+        <button type="submit" class="btn btn-default">Запросить</button>
+    </form>
+<div class="panels">
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h3 class="panel-title">Panel title</h3>
+        </div>
+        <div class="panel-body">
+            Panel content
+        </div>
+    </div>
+
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h3 class="panel-title">Panel title</h3>
+        </div>
+        <div class="panel-body">
+            Panel content
+        </div>
+    </div>
+
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h3 class="panel-title">Panel title</h3>
+        </div>
+        <div class="panel-body">
+            Panel content
+        </div>
+    </div>
+
+</div>
+
+
+</div>
+
+
+</body>
+</html>
